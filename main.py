91a143a5f395f232264c7bf7e7e3027cdbaedbde
MichalOlszewski <michu20011999@gmail.com>
2021-12-13 13:03:44 +0000

    changes

--- main.py
+++ main.py
@@ -37,11 +37,45 @@ def index():
     return "Let the battle begin!"
 
 @app.route("/", methods=['POST'])
+@app.route("/", methods=['POST'])
 def move():
-    request.get_data()
-    logger.info(request.json)
+    request_data = request.get_json()
+    MY_X = None
+    MY_Y = None
+    MY_DIRECTION = None
 
-    return moves[random.randint(0,len(moves))]
+    # logger.info(request.json)
+    # players = request_data["arena"]["state"].keys()
+
+    state = request_data["arena"]["state"]
+    for player, info in state.items():
+        if player == MY_SERVICE:
+            MY_X = info.get('x')
+            MY_Y = info.get('y')
+            MY_DIRECTION = info.get('direction')
+
+    for player, info in state.items():
+        players_y = info.get('y')
+        players_x = info.get('x')
+        if info.get('wasHit') is True:
+            continue
+        else:
+            if MY_X == players_x:
+                if players_y < MY_Y and MY_DIRECTION == 'N' and abs(players_y - MY_Y) <= 3:
+                    return "T"
+                elif players_y < MY_Y and MY_DIRECTION == 'S':
+                    return "L"
+                elif players_y > MY_Y and MY_DIRECTION == 'S' and abs(players_y - MY_Y) <= 3:
+                    return "T"
+            if MY_Y == players_y:
+                if players_x < MY_X and MY_DIRECTION == 'W' and abs(players_x - MY_X) <= 3:
+                    return "T"
+                elif players_x > MY_X and MY_DIRECTION == 'E' and abs(players_x - MY_X) <= 3:
+                    return "T"
+                elif players_x < MY_X and MY_DIRECTION == 'E':
+                    return 'L'
+
+    return moves[random.randrange(len(moves))]
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
