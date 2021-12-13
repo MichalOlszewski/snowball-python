00f6c6257dcf4d41557ae1b61673ce7e10242bf0
MichalOlszewski <michu20011999@gmail.com>
2021-12-13 11:56:44 +0000

    my changes

--- main.py
+++ main.py
@@ -18,11 +18,18 @@ import logging
 import random
 from flask import Flask, request
 
+
+
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-moves = ['F', 'T', 'L', 'R']
+moves = ['F', 'L', 'R', 'T']
+
+is_set = False
+throw = True
+move_me = "L"
+forward = True
 
 @app.route("/", methods=['GET'])
 def index():
@@ -30,9 +37,59 @@ def index():
 
 @app.route("/", methods=['POST'])
 def move():
+    global is_set
+    global throw
+    global forward
+    global move_me
     request.get_data()
     logger.info(request.json)
-    return moves[random.randrange(len(moves))]
+    if throw:
+        throw = False
+        return 'T'
+    throw = True
+    me = request.json["_links"]["self"]["href"]
+    x = request.json['arena']["state"][me]['x']
+    if x == 0:
+        is_set = True
+    y = request.json['arena']["state"][me]['y']
+    direction = request.json['arena']["state"][me]['direction']
+    size = request.json["arena"]["dims"]
+    size_y = size[1]
+    if y == 0:
+        move = "R"
+    if y == size_y or size_y - 1:
+        move = "L"
+    if not is_set:
+        if direction == "N" and x != 0:
+            return "L"
+        if direction == "S" and x != 0:
+            return 'R'
+        if direction == "W" and x != 0:
+            return 'F'
+        if direction == "W":
+            return 'R'
+        if direction == "N":
+            return "R"
+        if direction == "S":
+            return 'L'
+        if direction == "E" and x != 0:
+            return "L"
+    else:
+        if direction == "W" or direction == "E":
+            forward = True
+            return move
+        if direction == "N":
+            if forward:
+                forward = False
+                return "F"
+            return 'R'
+        if direction == "S":
+            if forward:
+                forward = False
+                return "F"
+            return "L"
+
+    return "T"
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
